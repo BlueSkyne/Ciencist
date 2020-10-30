1a194a1aca707c4dd58442a58365588867dfeacb
BlueSkyne <dressius@gmail.com>
2020-10-30 14:55:06 -0400

    subida de proyecto para trabajar

---
+++ TareaG-02 -v1/paquete_cientifico/nuevos_calculos.py
@@ -0,0 +1,29 @@
+#calculo del area y perimetro  de un circulo
+def area_circulo(radio):
+     area = 3.141516 * ( radio ** 2)
+     return area
+def  perimetro_circulo(radio):
+      perimetro = 2 *  3.141516 * radio
+      return perimetro  
+#calculo del area y perimetro de un triangulo
+def area_triangulo(base, altura):
+    area = (base*altura)/2
+    return area
+def perimetro_triangulo(lado1, lado2, lado3):
+    perimetro = lado1 + lado2 + lado3   
+    return perimetro
+#calculo del area y perimetro de un retangulo
+def area_rectangulo(largo, ancho):
+    area = largo * ancho
+    return area
+def perimetro_rectangulo(largo, ancho):
+    perimetro = (2*largo) + (2*ancho)  
+    return perimetro
+#calculo distancia recorrida expresada en Km y horas
+def distancia_recorrida(velocidad, tiempo):
+    distancia = velocidad * tiempo
+    return distancia
+
+
+#resultado = distancia_recorrida(120,8)
+#print(resultado)
