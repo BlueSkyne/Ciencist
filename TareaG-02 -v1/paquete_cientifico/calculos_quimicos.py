ea627d0ac9e552847906bb79e486a580ad1f62fc
BlueSkyne <dressius@gmail.com>
2020-10-30 16:40:22 -0400

    paquete quimico

---
+++ TareaG-02 -v1/paquete_cientifico/calculos_quimicos.py
@@ -0,0 +1,9 @@
+#Calculos quimicos
+#calculo de densida
+def densidad_cuerpo(volumen,masa):
+    densidad = masa / volumen
+    return densidad
+#conversion de centimetros cubicos a litros    
+def cc_litros(CC):
+    litros = CC  / 1000
+    return litros
